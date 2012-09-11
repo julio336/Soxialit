f6e83c1eacb530dc5d31c9a4133bd31abb030877
julio <julio336@hotmail.com>
2012-09-11 11:08:30 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soxialit::Application.config.secret_token = '95129484d9ea75927deaa72653e5a6e03acbedc8b9ec62dc2bee73f30ecdbe7d7da8f905731b9727db9a09724b8d538bf391164e7dbfb1e35a634868a4e583e5'
